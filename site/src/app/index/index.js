b91043e296df8b181acf0305ca0fa747b5aea588
quanta2015 <liyangtom@163.com>
2023-06-29 17:20:31 +0800

    添加q

--- site/src/app/index/index.js
+++ site/src/app/index/index.js
@@ -3,15 +3,17 @@ import React,{useEffect,useState} from 'react';
 import {Input, Table, Space, Pagination, Spin,Carousel} from 'antd'
 import {API_SERVER} from '@/constant/apis'
 import { observer,MobXProviderContext } from 'mobx-react'
-import { getNewsList,ltList,fdList,ptList,neList,qiList,qeList } from '@/constant/data'
+import { getNewsList,ltList,fdList,ptList,neList,qiList,qeList,qList } from '@/constant/data'
 import Card  from '@/component/Card'
-import CardQ from '@/component/CardQ'
+import CardQ1 from '@/component/CardQ1'
+import CardQ2 from '@/component/CardQ2'
 
 
 
 import s from './index.module.less';
 
 import icon_close from '@/img/close.svg'
+import icon_right from '@/img/right.svg'
 
 import slide01 from '@/img/slide01.png'
 import slide02 from '@/img/slide02.png'
@@ -33,6 +35,15 @@ const Index = () => {
   const listS = [1,1,1,1]
 
   const newsList = getNewsList()
+
+
+  const Header = (title)=>(
+    <div className={s.header}>
+      <h1>{title}</h1>
+      <span>{`${title}　一覧を見る`}</span>
+      <img src={icon_right} />
+    </div>
+  )
 
   return (
   
@@ -68,21 +79,21 @@ const Index = () => {
         <div className={s.adv}>ADVer</div>
 
         <section>
-          <h1>目撃情報</h1>
+          {Header('目撃情報')}
           <p>
             {ltList.map((item,i)=> <Card {...item} /> )}
           </p>
         </section>
 
         <section>
-          <h1>迷子情報</h1>
+          {Header('迷子情報')}
           <p>
             {fdList.map((item,i)=> <Card {...item} /> )}
           </p>
         </section>
 
         <section>
-          <h1>保護情報</h1>
+          {Header('保護情報')}
           <p>
             {ptList.map((item,i)=> <Card {...item} /> )}
           </p>
@@ -92,7 +103,7 @@ const Index = () => {
         <div className={s.adv}>ADVer</div>
 
         <section>
-          <h1>記事</h1>
+          {Header('記事')}
           <p>
             {neList.map((item,i)=> <Card {...item} /> )}
           </p>
@@ -100,20 +111,37 @@ const Index = () => {
 
 
         <section>
-          <h1>Q&A</h1>
+          {Header('Q&A')}
           <p>
             <div className={s.wrap}>
               <span className={s.title}>受付中の質問</span>
-              {qiList.map((item,i)=> <CardQ {...item} clr={'#33831F'} /> )}
+              {qiList.map((item,i)=> <CardQ1 {...item} clr={'#33831F'} /> )}
             </div>
             <div className={s.wrap}>
               <span className={s.title}>受付中の質問</span>
-              {qeList.map((item,i)=> <CardQ {...item} clr={'#DE5A5A'} /> )}
+              {qeList.map((item,i)=> <CardQ1 {...item} clr={'#DE5A5A'} fit={'var(--fil-grey)'} /> )}
             </div>
           </p>
         </section>
 
         <div className={s.adv}>ADVer</div>
+
+
+          
+        <section>
+          {Header('記事 ランキング')}
+          <p>
+            {neList.map((item,i)=> <Card {...item} id={i+1} /> )}
+          </p>
+        </section>
+
+
+        <section>
+          {Header('Q&A ランキング')}
+          <p>
+            {qList.map((item,i)=> <CardQ2 {...item} id={i+1} /> )}
+          </p>
+        </section>
 
       </div>
       
