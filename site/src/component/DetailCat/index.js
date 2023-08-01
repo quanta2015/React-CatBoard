f000e7dd5ac33282e47e7bc30331f98c2aa49135
quanta2015 <liyangtom@163.com>
2023-08-01 14:41:08 +0800

    CardDetailCatコンポーネントの作成

--- site/src/component/DetailCat/index.js
+++ site/src/component/DetailCat/index.js
@@ -3,6 +3,7 @@ import { observer,MobXProviderContext } from 'mobx-react'
 import classnames from 'classnames';
 import { Button, Form, Input, Radio,Select,Upload,Modal,DatePicker,TimePicker } from 'antd';
 import {PRE_IMG} from '@/constant/urls'
+import CardDetailCat from '@/component/CardDetailCat'
 import s from './index.module.less';
 
 
@@ -15,7 +16,7 @@ import icon_rt  from '@/img/icon/right.svg'
 const Detail = ({}) => {
   const { store } = React.useContext(MobXProviderContext)
 
-  const { cat,type,sub_date,sub_user,addr,title,sub,period,view,fav,id } = store.item
+  const { cat,type,sub_date,sub_user,addr,title,sub,content,period,view,fav,id } = store.item
   const { age,attr,clr,image,img,name,sex,size,status } = cat
 
 
@@ -34,6 +35,9 @@ const Detail = ({}) => {
     setCurImg(img[cur]);
   }
 
+  const doSendMsg =()=>{
+    // console.log('msg')
+  }
 
   return (
     <div className={s.detail} >
@@ -42,91 +46,9 @@ const Detail = ({}) => {
         <div className={'del'} onClick={doClose}></div>
 
 
-        <div className={s.wrap}>
-
-          
-          <div className={s.hd}>
-            <div className={s.item}>
-              <img src={icon_usr} />
-              {sub_user}
-            </div>
-            <div className={s.item}>
-              <img src={icon_map} />
-              {`${addr?.addr_ken}${addr?.addr_shi}${addr?.addr_dtl}`}
-            </div>
-            <div className={s.item}>
-              <img src={icon_cal} />
-              {sub_date}
-            </div>
-          </div>
+        <CardDetailCat btnTxt={'メッセージを送る'} btnEvent={doSendMsg}/>
 
 
-          <div className={s.bd}>
-            <div className={s.img}>
-              <img className={s.arrow} src={icon_lt} onClick={()=>doSel(-1)} />
-              <div className={s.list}>
-                {img.map((item,i)=>
-                  <img src={`${PRE_IMG}${curImg}`} key={i} className={i===sel?'act':''} />
-                )}
-              </div>
-              <img className={s.arrow} src={icon_rt} onClick={()=>doSel(1)} />
-            </div>
-   
-   
-            <div className={s.sig}>
-              <div className={s.row}>
-                <label>名前</label>
-                <span>{name}</span>
-              </div>
-              <div className={s.row}>
-                <label>毛色</label>
-                <span>{clr}</span>
-              </div>
-              <div className={s.row}>
-                <label>猫種</label>
-                <span>{type}</span>
-              </div>
-              <div className={s.row}>
-                <label>首輪</label>
-                <span>青色の首輪</span>
-              </div>
-              <div className={s.row}>
-                <label>性別</label>
-                <span>{sex}</span>
-              </div>
-              <div className={s.row}>
-                <label>年齢</label>
-                <span>{age}</span>
-              </div>
-              <div className={s.row}>
-                <label>大きさ</label>
-                <span>{size}</span>
-              </div>
-            </div>
-
-
-            <div className={s.com}>
-              <div className={s.row}>
-                <label>その他特徴</label>
-                <p>尻尾が短いです。サクラ耳です。痩せています。とても大人しくてフレンドリーな猫ちゃんです。</p>
-              </div>
-              
-              <div className={s.row}>
-                <label>保護した時の状況</label>
-                <p>近所で座り込んでいるのを見つけました。普段見ない猫ちゃんなので近寄ってみたら懐いてくれました。元気がなさそうだったでドライフードを与えてみましたが、中々食べてくれません。今は自宅の中で保護しています。</p>
-              </div>
-            </div>
-
-          </div>
-
-          <div className={s.ft}>
-            
-            <div className={classnames(s.btn,type) }>
-              メッセージを送る
-            </div>
-          </div>
-  　
-        </div>
       </div>
     </div>
   )
